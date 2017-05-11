f4f8d81e7efeeab310e46cb8c64e384bc9a604b9
Y.N Pamungkas Jayuda <yulius.Jayuda@gmail.com>
2017-05-11 16:20:50 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+var app       = require('app');
+var browser   = require('browser-window');
+var electrify = require('electrify')(__dirname);
+
+var window    = null;
+
+app.on('ready', function() {
+    process.env.MONGO_URL="mongodb://103.253.107.113:10010/ERP";
+    process.env.ROOT_URL="http://103.253.107.113:10011";
+
+  // electrify start
+  electrify.start(function(meteor_root_url) {
+
+    // creates a new electron window
+    window = new browser({
+      width: 1000, height: 600,
+      'node-integration': false // node integration must to be off
+    });
+
+    // open up meteor root url
+    window.loadURL(meteor_root_url);
+  });
+});
+
+
+app.on('window-all-closed', function() {
+  app.quit();
+});
+
+
+app.on('will-quit', function terminate_and_quit(event) {
+  
+  // if electrify is up, cancel exiting with `preventDefault`,
+  // so we can terminate electrify gracefully without leaving child
+  // processes hanging in background
+  if(electrify.isup() && event) {
+
+    // holds electron termination
+    event.preventDefault();
+
+    // gracefully stops electrify 
+    electrify.stop(function(){
+
+      // and then finally quit app
+      app.quit();
+    });
+  }
+});
+
