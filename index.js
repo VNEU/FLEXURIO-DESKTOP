e6924536c88617fa83c96f1688cf35453d925744
Y.N Pamungkas Jayuda <yulius.jayuda@gmail.com>
2017-05-13 07:29:10 +0700

    edit local mongodb By Jayuda From ThinkMac

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var electrify = require('electrify')(__dirname);
 var window    = null;
 
 app.on('ready', function() {
-    process.env.MONGO_URL="mongodb://[USERNAME]:[PASSWORD]@[YOUR IP MONGODB]:[YOURPORT]/[YOURDB]";
-    process.env.ROOT_URL="http://[YOUR ROOT URL FLEXURIO]";
+    process.env.MONGO_URL="mongodb://localhost:3001/admin";
+    process.env.ROOT_URL="http://localhost";
 
   // electrify start
   electrify.start(function(meteor_root_url) {
@@ -29,7 +29,7 @@ app.on('window-all-closed', function() {
 
 
 app.on('will-quit', function terminate_and_quit(event) {
-  
+
   // if electrify is up, cancel exiting with `preventDefault`,
   // so we can terminate electrify gracefully without leaving child
   // processes hanging in background
@@ -38,7 +38,7 @@ app.on('will-quit', function terminate_and_quit(event) {
     // holds electron termination
     event.preventDefault();
 
-    // gracefully stops electrify 
+    // gracefully stops electrify
     electrify.stop(function(){
 
       // and then finally quit app
@@ -46,4 +46,3 @@ app.on('will-quit', function terminate_and_quit(event) {
     });
   }
 });
-
